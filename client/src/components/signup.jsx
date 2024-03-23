35ffd0c6f1ceb364ee192ce98d521782b8c124b5
unknown <ehabzaferabed@gmail.com>
2024-03-23 10:08:54 +0300

    edit regex

--- client/src/components/signup.jsx
+++ client/src/components/signup.jsx
@@ -65,7 +65,7 @@ function Register() {
 
   const validatePassword = (password) => {
     const passwordPattern =
-    /^[A-Za-z\s\d+]{6,30}$/;
+    /^[A-Za-z0-9@#]*$/;
     return passwordPattern.test(password);
   };
   const validateFirstName = (name) => {
